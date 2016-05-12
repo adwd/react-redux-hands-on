0fe08e6a6d9d69cf5350c1e1fe890e5d184816ab
西田 雅博 <masahiro.nishida@bizreach.co.jp>
2016-05-12 17:42:58 +0900

    remove unused files

--- server/api/index.js
+++ server/api/index.js
@@ -1,14 +1,10 @@
 import Router from 'koa-router'
-import usersApi from './users'
-import eventsApi from './events'
 import todosApi from './todos'
 
 const api = new Router({
   prefix: '/api'
 })
 
-api.use('/users', usersApi.routes())
-api.use('/events', eventsApi.routes())
 api.use('/todos', todosApi.routes())
 
 export default api
