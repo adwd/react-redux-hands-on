efe242a072ca1d0a944c8b34fb0a13568810af66
西田 雅博 <masahiro.nishida@bizreach.co.jp>
2016-05-17 17:34:48 +0900

    avoid using stateless function component

--- src/views/ReactSample/TodoItem.js
+++ src/views/ReactSample/TodoItem.js
@@ -1,20 +1,21 @@
-import React, { PropTypes } from 'react'
+import React, { Component, PropTypes } from 'react'
 
-// 状態を持たないコンポーネントはただの関数として定義できる
-// https://facebook.github.io/react/docs/reusable-components.html#stateless-functions
-const TodoItem = ({ index, onRemove, children }) => (
-  <div>
-    {`${index + 1}`}: {children} <input type='button' value='x' onClick={() => onRemove(index)} />
-  </div>
-)
-
-TodoItem.propTypes = {
-  index: PropTypes.number,
-  onRemove: PropTypes.func,
-  children: PropTypes.oneOfType([
-    React.PropTypes.arrayOf(React.PropTypes.node),
-    React.PropTypes.node
-  ])
+export default class TodoItem extends Component {
+  static propTypes = {
+    index: PropTypes.number,
+    onRemove: PropTypes.func,
+    children: PropTypes.oneOfType([
+      React.PropTypes.arrayOf(React.PropTypes.node),
+      React.PropTypes.node
+    ])
+  }
+  
+  render() {
+    const { index, onRemove, children } = this.props;
+    return (
+      <div>
+        {`${index + 1}`}: {children} <input type='button' value='x' onClick={() => onRemove(index)} />
+      </div>
+    );
+  }
 }
-
-export default TodoItem
