31a751a71e280aef9dca01167af692013f5eda58
西田 雅博 <masahiro.nishida@bizreach.co.jp>
2016-07-01 11:33:25 +0900

    slight improvement in material-ui test code

--- tests/layouts/CoreLayout.spec.js
+++ tests/layouts/CoreLayout.spec.js
@@ -2,12 +2,13 @@ import React from 'react'
 import { shallow } from 'enzyme'
 import assert from 'power-assert'
 import CoreLayout from 'layouts/CoreLayout/CoreLayout'
-import extendComponentWithMUITheme from '../extendComponentWithMUITheme'
+import { getMuiTheme } from 'material-ui/styles'
 
 describe('(Layout) Core', function () {
+  const muiTheme = getMuiTheme()
+  
   it('Should render.', function () {
-    const Extended = extendComponentWithMUITheme(CoreLayout)
-    const wrapper = shallow(<Extended />)
+    const wrapper = shallow(<CoreLayout />, { context: { muiTheme } })
     assert(wrapper.first().is('CoreLayout'))
   })
 })
